8a3466f73ab1797c2642307c5ec899796336d79a
Annjoynj <135809579+Annjoynj@users.noreply.github.com>
2023-06-21 11:42:16 +0300

    Add files via upload

--- create_tables.py
+++ create_tables.py
@@ -7,3 +7,7 @@ app.app_context().push()
 
 with app.app_context():
     db.create_all()
+    users = User.query.all()
+    for user in users:
+        print(user.email, user.first_name, user.last_name)
+
